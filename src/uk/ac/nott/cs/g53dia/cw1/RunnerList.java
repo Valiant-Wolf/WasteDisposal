da3fd0f992de7f41bae508262ef5bdc9203d051e
Jayme Green <valiant_wolf@hotmail.co.uk>
2017-03-15 16:54:38 +0000

    Document RunnerList

--- src/uk/ac/nott/cs/g53dia/cw1/RunnerList.java
+++ src/uk/ac/nott/cs/g53dia/cw1/RunnerList.java
@@ -12,6 +12,10 @@ public class RunnerList<E> {
 	private Stack<Bookmark> bookmarks = new Stack<>();
 	private int position = 0;
 
+	/**
+	 * Moves the iteration head forwards until the next entry has the given key or higher
+	 * @param position the key to move up to
+	 */
 	private void walkForwardTo(int position) {
 		boolean nextNull = false;
 		boolean moved = false;
@@ -28,6 +32,10 @@ public class RunnerList<E> {
 		if (!nextNull && moved) previous = next.previous;
 	}
 
+	/**
+	 * Moves the iteration head backwards until the next entry has the given key or higher
+	 * @param position the key to move down past
+	 */
 	private void walkBackwardTo(int position) {
 		boolean prevNull = false;
 		boolean moved = false;
@@ -44,12 +52,23 @@ public class RunnerList<E> {
 		if (!prevNull && moved) next = previous.next;
 	}
 
+	/**
+	 * Positions the iteration head at the given key, such that all entries with the given key are
+	 * ahead of it
+	 * @param position the key to move to
+	 */
 	void moveTo(int position) {
 		 walkForwardTo(position);
 		 walkBackwardTo(position);
 		 this.position = position;
 	}
 
+	/**
+	 * Adds an entry to the correct position in the list with the given key. Duplicate keys are
+	 * permitted
+	 * @param key the position of the entry in the list
+	 * @param value the value of the entry
+	 */
 	void add(int key, E value) {
 		pushBookmark();
 
@@ -61,6 +80,13 @@ public class RunnerList<E> {
 		popBookmark(newEntry);
 	}
 
+	/**
+	 * Removes all entries with the given key and value. Comparisons are made with the value's
+	 * equals() method.
+	 * @param key the key to remove entries at
+	 * @param value the value of the entries to remove
+	 * @return true if any number of entries were removed, false otherwise
+	 */
 	boolean remove(int key, E value) {
 		pushBookmark();
 
@@ -82,6 +108,10 @@ public class RunnerList<E> {
 		return removed;
 	}
 
+	/**
+	 * Adds all of the provided entries to the list in their correct positions
+	 * @param entries the entries to add
+	 */
 	void addAll(List<Entry<E>> entries) {
 		if (entries.size() == 0) return;
 
@@ -102,6 +132,12 @@ public class RunnerList<E> {
 		popBookmark(next);
 	}
 
+	/**
+	 * Retrieves all entries with a key surrounding or equal to the current position. If there are
+	 * entries at keys 1, 2, 4, 5, 7 and the current position is 4, all entries with a key of 2, 4
+	 * or 5 will be returned
+	 * @return the entries adjacent to the current position
+	 */
 	List<E> getAdjacent() {
 		LinkedList<E> list = new LinkedList<>();
 
@@ -136,6 +172,13 @@ public class RunnerList<E> {
 		return list;
 	}
 
+	/**
+	 * Retrieves all entries with a key within a certain distance from a centre position. All
+	 * entries with a key such that (centre - radius <= key <= centre + radius) will be returned
+	 * @param centre the centre of the search
+	 * @param radius the radius of the search
+	 * @return all entries with a key within radius of the centre
+	 */
 	List<E> getAllInRange(int centre, int radius) {
 		LinkedList<E> list = new LinkedList<>();
 
@@ -154,10 +197,17 @@ public class RunnerList<E> {
 		return list;
 	}
 
+	/**
+	 * Stores the current position of the iteration head for recall
+	 */
 	private void pushBookmark() {
 		bookmarks.push(new Bookmark());
 	}
 
+	/**
+	 * Recalls the most recently stored iteration head
+	 * @param fallbackNext the new Entry to use if the list was empty on the last push call
+	 */
 	private void popBookmark(Entry<E> fallbackNext) {
 		Bookmark bookmark = bookmarks.pop();
 		if (bookmark.isNext) {
@@ -170,6 +220,10 @@ public class RunnerList<E> {
 		moveTo(bookmark.pos);
 	}
 
+	/**
+	 * Removes the given Entry from all bookmarks to facilitate correct removal
+	 * @param entry the Entry to remove
+	 */
 	private void removeFromBookmarks(Entry<E> entry) {
 		for (Bookmark bookmark : bookmarks) {
 			if (entry.equals(bookmark.entry)) {
@@ -195,6 +249,10 @@ public class RunnerList<E> {
 		}
 	}
 
+	/**
+	 * Represents an entry in a RunnerList with a key and a value
+	 * @param <V> the type of the value
+	 */
 	public static class Entry<V> implements Comparable<Entry> {
 
 		final int key;
@@ -220,6 +278,9 @@ public class RunnerList<E> {
 
 	}
 
+	/**
+	 * Represents a stored iteration position
+	 */
 	private class Bookmark {
 
 		Entry<E> entry;
