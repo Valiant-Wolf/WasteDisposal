d53e8f0c916eefecdba7b863e9965cab60ca74b2
Jayme Green <valiant_wolf@hotmail.co.uk>
2017-03-15 17:26:27 +0000

    Document WanderPlan

--- src/uk/ac/nott/cs/g53dia/cw1/plans/WanderPlan.java
+++ src/uk/ac/nott/cs/g53dia/cw1/plans/WanderPlan.java
@@ -17,22 +17,27 @@ public class WanderPlan extends TankerPlan {
 	public WanderPlan(MyTanker tanker, List<TankerEvent> events) {
 		super(tanker, events);
 
+		// If the Tanker's fuel is critical, this plan is invalid
 		for (TankerEvent event : events) {
 			if (event instanceof CriticalFuelEvent) return;
 		}
 
+		// Add a WanderAction to the queue
 		actionQueue.add(action);
 	}
 
 	@Override
 	public Action getNextAction() {
+		// Return but do not remove the WanderAction from the queue
 		return actionQueue.peek();
 	}
 
 	@Override
 	public boolean checkValidity(List<TankerEvent> events) {
 		for (TankerEvent event : events) {
+			// Critical fuel instantly invalidates this plan
 			if (event instanceof CriticalFuelEvent) return false;
+			// Any incomplete Task invalidates this plan
 			if (event instanceof TaskEvent) return false;
 			if (event instanceof PendingTaskEvent) return false;
 		}
