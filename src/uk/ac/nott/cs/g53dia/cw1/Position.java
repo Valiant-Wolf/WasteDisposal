b1552d8474f0e3ed7de811839d5ebff9f037a131
Jayme Green <valiant_wolf@hotmail.co.uk>
2017-03-15 16:38:46 +0000

    Document Position

--- src/uk/ac/nott/cs/g53dia/cw1/Position.java
+++ src/uk/ac/nott/cs/g53dia/cw1/Position.java
@@ -2,22 +2,47 @@ package uk.ac.nott.cs.g53dia.cw1;
 
 import java.util.Set;
 
+/**
+ * Represents an offset from the environment's origin (the Tanker's initial position)
+ */
 public class Position {
 
+	/**
+	 * The X offset of this Position
+	 */
 	public int x;
+
+	/**
+	 * The Y offset of this Position
+	 */
 	public int y;
 
+	/**
+	 * Creates a new instance with the given x and y offsets
+	 * @param x the x offset
+	 * @param y the y offset
+	 */
 	public Position(int x, int y) {
 		this.x = x;
 		this.y = y;
 	}
 
+	/**
+	 * Calculates the Chebyshev distance between this and another Position
+	 * @param that the Position to find the distance to
+	 * @return the Chebyshev distance between the two Positions
+	 */
 	public int distanceTo(Position that) {
 		int dx = Math.abs(that.x - this.x);
 		int dy = Math.abs(that.y - this.y);
 		return Math.max(dx, dy);
 	}
 
+	/**
+	 * Calculates the direction of the line between this and another Position
+	 * @param target the Position to find the direction to
+	 * @return the Direction of the other Position relative to this Position
+	 */
 	public Direction directionTowards(Position target) {
 		int xDiff = target.x - this.x;
 		int xAbs = Math.abs(xDiff);
@@ -39,6 +64,11 @@ public class Position {
 		return Direction.fromInt(intDir);
 	}
 
+	/**
+	 * Returns whether this and another Position represent the same point
+	 * @param obj the other Position
+	 * @return true if this and another Position represent the same point, false otherwise
+	 */
 	@Override
 	public boolean equals(Object obj) {
 		if (!(obj instanceof Position)) return false;
@@ -47,11 +77,22 @@ public class Position {
 		return (this.x == that.x && this.y == that.y);
 	}
 
+	/**
+	 * Creates a hashcode consistent with equals(). Two equivalent Positions will produce the same
+	 * hashcode
+	 * @return an integer hashcode for this Position
+	 */
 	@Override
 	public int hashCode() {
 		return (((x & 0xff) << 16) + (y & 0xff));
 	}
 
+	/**
+	 * Finds the nearest Position and its distance from a target Position from a set of Positions
+	 * @param target the Position to measure from
+	 * @param positions the set of Positions from which the result should be chosen
+	 * @return the nearest Position and its distance
+	 */
 	public static NearestResult getNearest(Position target, Set<? extends Position> positions) {
 		int minDist = Integer.MAX_VALUE;
 		Position closest = null;
@@ -66,6 +107,9 @@ public class Position {
 		return new NearestResult(closest, minDist);
 	}
 
+	/**
+	 * Encapsulates a Position and a distance
+	 */
 	public static class NearestResult {
 
 		public final Position position;
